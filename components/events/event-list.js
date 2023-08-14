603ea5b37e37fe6f26ce9dbfad951f675f3c4814
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-08-14 21:04:03 +0300

    style events components

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,8 +1,9 @@
 import EventItem from "./event-item";
+import classes from "./event-list.module.css";
 
 function EventList(props) {
   return (
-    <ul>
+    <ul className={classes.list}>
       {props.events.map((event) => (
         <EventItem
           title={event.title}
