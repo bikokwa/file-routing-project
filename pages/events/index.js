a378d9d74d694ea537bf826a7a58edc15c0247fc
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-08-16 17:22:06 +0300

    navigate to filtered events page

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,20 @@
+import { useRouter } from "next/router";
 import EventList from "../../components/events/event-list";
 import EventsSearch from "../../components/events/events-search";
 import { getAllEvents } from "../../dummy-data";
 
 function AllEventsPage() {
+  const router = useRouter();
   const events = getAllEvents();
+
+  function filteredEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={filteredEventsHandler} />
       <EventList events={events} />
     </>
   );
