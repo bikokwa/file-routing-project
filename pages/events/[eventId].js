4a64613b61acc9762f69447a87b2a51b11cd92bc
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-08-21 18:46:41 +0300

    optimize data fetching

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,4 +1,4 @@
-import { getAllEvents, getEventById } from "../../helpers/api-util";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
@@ -9,9 +9,9 @@ function EventDetailPage(props) {
 
   if (!event) {
     return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
+      <div className="center">
+        <p>Loading...</p>
+      </div>
     );
   }
 
@@ -32,11 +32,11 @@ function EventDetailPage(props) {
 }
 
 export async function getStaticPaths() {
-  const events = await getAllEvents();
+  const events = await getFeaturedEvents();
   const paths = events.map((event) => ({ params: { eventId: event.id } }));
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths,
   };
 }
@@ -49,6 +49,7 @@ export async function getStaticProps(context) {
     props: {
       event,
     },
+    revalidate: 30,
   };
 }
 
