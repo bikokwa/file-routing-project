547a3b8648a333558c461d405c04ca038bfc9b4b
bikokwa <34458781+bikokwa@users.noreply.github.com>
2023-08-21 18:33:02 +0300

    load data and paths for dynamic pages

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,14 +1,11 @@
-import { useRouter } from "next/router";
-import { getEventById } from "../../dummy-data";
+import { getAllEvents, getEventById } from "../../helpers/api-util";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/ui/error-alert";
 
-function EventDetailPage() {
-  const router = useRouter();
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+function EventDetailPage(props) {
+  const { event } = props;
 
   if (!event) {
     return (
@@ -32,6 +29,27 @@ function EventDetailPage() {
       </EventContent>
     </>
   );
+}
+
+export async function getStaticPaths() {
+  const events = await getAllEvents();
+  const paths = events.map((event) => ({ params: { eventId: event.id } }));
+
+  return {
+    fallback: false,
+    paths,
+  };
+}
+
+export async function getStaticProps(context) {
+  const eventId = context.params.eventId;
+  const event = await getEventById(eventId);
+
+  return {
+    props: {
+      event,
+    },
+  };
 }
 
 export default EventDetailPage;
